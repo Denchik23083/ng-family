3642f0ba07f0bca36dbbcfcc2e95de60a44c17c1
Denchik23083 <deniskudravov228@gmail.com>
2023-12-17 11:14:24 +0200

    admin, god template is done

---
+++ src/app/components/user/user-to-admin/user-to-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-to-admin',
+  templateUrl: './user-to-admin.component.html',
+  styleUrls: ['./user-to-admin.component.scss']
+})
+export class UserToAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
